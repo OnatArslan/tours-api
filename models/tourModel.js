60118009739aca2f27b39fe43d7cdf99655bdbde
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-06-18 22:41:55 +0300

    Commit for update Tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,7 +5,28 @@ const toursSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, `A tour must have a price`],
-    unique: [true, `A user must have unique name`]
+    unique: [true, `A user must have unique name`],
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true, `A tour must have a duration`]
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, `A tour must have a group size`]
+  },
+  difficult: {
+    type: String,
+    required: [true, `A tour must have a difficulty`]
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   rating: {
     type: Number,
@@ -14,6 +35,32 @@ const toursSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: [true, `A tour must have a price`]
+  },
+  priceDiscount: {
+    type: Number
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, `A tour must have a cover image`]
+  },
+  images: {
+    type: [String]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: {
+    type: [Date]
   }
 });
 // Creating a model based on Tours schema
