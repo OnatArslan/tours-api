6bf9c3c7a3ca6e2b07ce7b7dd584a1c6cfa092a9
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-06-20 09:33:27 +0300

    Commit for error controller.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const AppError = require(`./utils/appError`);
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const globalErrorHandler = require(`./controllers/errorController`);
+
 const app = express();
 
 // 1) MIDDLEWARES
@@ -33,14 +35,6 @@ app.all(`*`, (req, res, next) => {
 });
 
 // ERROR HANDLER MIDDLEWARE
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || `error`;
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
