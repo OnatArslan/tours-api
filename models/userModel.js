88521429639d8dd8d359361b5dcab084bae3af8c
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-06-21 03:36:29 +0300

    Commit for hashing password with pre save middleware

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name email photo password passwordConfirm
 const userSchema = new mongoose.Schema(
@@ -27,7 +28,14 @@ const userSchema = new mongoose.Schema(
     passwordConfirm: {
       type: String,
       required: [true, `Please confirm your password`],
-      minLength: [8, `Passoword must include at least 8 characters`]
+      minLength: [8, `Passoword must include at least 8 characters`],
+      validate: {
+        validator: function(val) {
+          //This is only work for save() or create() method not update()
+          return val === this.password;
+        },
+        message: `Passwords are not match`
+      }
     },
     photo: {
       type: String
@@ -35,6 +43,30 @@ const userSchema = new mongoose.Schema(
   },
   {}
 );
+
+// HASH THE PASSWORD WITH PRE SAVE MIDDLEWARE
+// Define a pre-save middleware for the user schema.
+// This middleware will execute before a user document is saved to the database.
+userSchema.pre(`save`, async function(next) {
+  // Check if the password field has been modified.
+  // If not, skip the hashing process and proceed to the next middleware or save operation.
+  // This is to avoid re-hashing the password if it hasn't changed.
+  if (!this.isModified(`password`)) {
+    return next();
+  }
+
+  // If the password is modified, hash it using bcrypt with a cost factor of 12.
+  // A higher cost factor increases the hashing time, enhancing security but requiring more processing power.
+  // The hashed password replaces the plain text password in the document.
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Remove the passwordConfirm field from the document.
+  // This field is used for validation purposes only and should not be stored in the database.
+  this.passwordConfirm = undefined;
+
+  // Proceed to the next middleware or complete the save operation.
+  next();
+});
 
 const User = mongoose.model(`User`, userSchema);
 
